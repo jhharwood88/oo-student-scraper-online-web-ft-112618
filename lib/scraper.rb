3d45efa5f7002b312111cdf8eebd62172c995921
Jonathan Harwood <jhharwood88@gmail.com>
2019-01-09 05:23:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -36,11 +36,11 @@ class Scraper
       if href_var.include? "twitter.com"
         holder_hash << {:twitter => href_var}
       elsif href_var.include? "linkedin.com"
-        holder_hash << {:linkedin => href_var
+        holder_hash << {:linkedin => href_var}
       elsif href_var.include? "github.com"
-        holder_hash << :github => href_var
+        holder_hash << {:github => href_var}
       else 
-        holder_hash << :blog => href_var
+        holder_hash << {:blog => href_var}
     end
    end
   end
