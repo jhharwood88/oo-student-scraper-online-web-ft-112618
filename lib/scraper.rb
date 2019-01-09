c38c6e677061e79c724d0ab1b702d82318cbdb3d
Jonathan Harwood <jhharwood88@gmail.com>
2019-01-09 04:56:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,27 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
-
+  
+  
   def self.scrape_index_page(index_url)
     
+    scraped_students = []
+    
+    doc = Nokogiri::HTML(open(index_url))
+    
+    doc.css(".student-card").each do |student|
+    scraped_students << {:name => student.css("h4").text , :location => student.css("p").text , :profile_url => student.css("a")[0]["href"]}
+    end
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
     
+    profile_doc = Nokogiri::HTML(open(profile_url)) 
+    
+    profile_doc.css(".social-icon-container").each do |profile|
+    
+    binding.pry
   end
-
-end
-
